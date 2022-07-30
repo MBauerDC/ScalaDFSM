f294da79f2cc0db7e6ee098785be719f862c8c1a
MBauerDC <33489577+MBauerDC@users.noreply.github.com>
2022-07-30 16:34:29 +0200

    add computed value

--- sketch.scala
+++ sketch.scala
@@ -1,12 +1,17 @@
 type FSMName = String
-type FSMIdentifier = String;
+
+type FSMIdentifier = String
+
+type FSMComputedValue = Option[Any]
+
 trait FSMState[N <: FSMName]:
   val identifier: FSMIdentifier
   type MachineName = N
+  def getComputedValue(previousComputedValue: FSMComputedValue, transitionHistory: Seq[FSMState[N]]): FSMComputedValue
+
 trait FSMInput[N <: FSMName]:
   val identifier: FSMIdentifier
   type MachineName = N
-
 
 trait FiniteStateMachine[N <: FSMName](
   val acceptedInputs: Set[FSMInput[N]],
@@ -21,6 +26,7 @@ trait FiniteStateMachine[N <: FSMName](
 trait FiniteStateMachineInstance[N <: FSMName] extends FiniteStateMachine[N]:
   val currentState: FSMState[N]
   val transitionHistory: Seq[FSMState[N]]
+  val computedValue: Option[Any]
   def isInAcceptingState: Boolean
   def isInRejectingState: Boolean
   def applyInput(input: FSMInput[N]): FiniteStateMachineInstance[N]
@@ -34,7 +40,8 @@ class GenericFiniteStateMachine[N <: FSMName]
   override val rejectingStates: Set[FSMState[N]],
   override val acceptingStates: Set[FSMState[N]],
   val currentState: FSMState[N],
-  val transitionHistory: Seq[FSMState[N]]
+  val transitionHistory: Seq[FSMState[N]],
+  val computedValue: FSMComputedValue
 ) extends FiniteStateMachine[N](
   acceptedInputs,
   states,
@@ -44,7 +51,14 @@ class GenericFiniteStateMachine[N <: FSMName]
   acceptingStates
 ) with FiniteStateMachineInstance[N]:
   def applyInput(input:FSMInput[N]) = 
-    val newCurrentState = transitionFn(currentState, input)
+    val newCurrentState = if !this.isInRejectingState then transitionFn(currentState, input) else currentState
+    val newTransitionHistory = if !this.isInRejectingState then transitionHistory :+ newCurrentState else transitionHistory
+    val intermediateComputedValue = 
+      computedValue match
+        case None => Some(List(s"Got input: ${input}"))
+        case Some(str:Seq[String]) => Some(str :+ s"Got input: ${input}")
+        case Some(any: Any) => Some((any, s"Got input: ${input}"))
+    val newComputedValue = newCurrentState.getComputedValue(intermediateComputedValue, newTransitionHistory)
     new GenericFiniteStateMachine(
       acceptedInputs,
       states,
@@ -53,7 +67,8 @@ class GenericFiniteStateMachine[N <: FSMName]
       rejectingStates,
       acceptingStates,
       newCurrentState,
-      transitionHistory :+ newCurrentState
+      newTransitionHistory,
+      newComputedValue
     )
   def isInAcceptingState: Boolean = 
     acceptingStates.contains(currentState)
@@ -68,6 +83,9 @@ object GenericFiniteStateMachine{
     transitionFn: (FSMState[N], FSMInput[N]) => FSMState[N],
     rejectingStates: Set[FSMState[N]],
     acceptingStates: Set[FSMState[N]],
+    currentState: FSMState[N],
+    transitionHistory: Seq[FSMState[N]],
+    computedValue: FSMComputedValue
   ): GenericFiniteStateMachine[N] = 
     new GenericFiniteStateMachine[N](
       acceptedInputs,
@@ -76,8 +94,9 @@ object GenericFiniteStateMachine{
       transitionFn,
       rejectingStates,
       acceptingStates,
-      initialState,
-      Seq(initialState)
+      currentState,
+      transitionHistory,
+      computedValue
     )
 }
 
@@ -85,6 +104,12 @@ object GenericFiniteStateMachine{
 type FSM1Name = "FSM1";
 val FSM1InitialState: FSMState[FSM1Name] = new FSMState[FSM1Name]{ 
   val identifier = "FSM1Init"
+  def getComputedValue(previousComputedValue: FSMComputedValue, transitionHistory: Seq[FSMState[FSM1Name]]): FSMComputedValue = 
+    previousComputedValue match 
+      case None => Some(Seq[String]("Machine is in InitialState"))
+      case Some(s:Seq[String]) => Some(s :+ "Machine is in InitialState")
+      case Some(s:Seq[Any]) => Some(s :+ "Machine is in InitialState")
+      case Some(previousValue:Any) => Some((previousValue, "Machine is in InitialState"))
 }
 val FSM1InputA: FSMInput[FSM1Name] = new FSMInput[FSM1Name]{
   val identifier = "FSM1InputA"
@@ -100,11 +125,24 @@ val anyToFSM1Input: (Any) => FSMInput[FSM1Name] =
       case _ => FSM1InputElse
   }
 
+trait FSMError
+case class FSMInRejectingState[N <: FSMName](state: FSMState[N], transitionHistory: Seq[FSMState[N]]) extends FSMError
+
 val FSM1RejectState: FSMState[FSM1Name] = new FSMState[FSM1Name]{
   val identifier = "FSM1Rejecting"
+  def getComputedValue(
+    previousComputedValue: FSMComputedValue, 
+    transitionHistory: Seq[FSMState[FSM1Name]]
+  ) = Some(FSMInRejectingState[FSM1Name](this, transitionHistory))
 }
 
 val FSM1AcceptState: FSMState[FSM1Name] = new FSMState[FSM1Name]{
+  def getComputedValue(previousComputedValue: FSMComputedValue, transitionHistory: Seq[FSMState[FSM1Name]]): FSMComputedValue = 
+    previousComputedValue match 
+      case None => Some(Seq[String]("Machine is in AcceptingState"))
+      case Some(s:Seq[String]) => Some(s :+ "Machine is in AcceptingState")
+      case Some(s:Seq[Any]) => Some(s :+ "Machine is in AcceptingState")
+      case Some(previousValue:Any) => Some((previousValue, "Machine is in AcceptingState"))
   val identifier = "FSM1Accepting"
 }
 val FSM1States: Set[FSMState[FSM1Name]] = Set(FSM1InitialState, FSM1RejectState, FSM1AcceptState)
@@ -125,13 +163,22 @@ var FSM1Instance: FiniteStateMachineInstance[FSM1Name] = GenericFiniteStateMachi
   FSM1InitialState,
   FSM1TransitionFunction,
   FSM1RejectingStates,
-  FSM1AcceptingStates  
+  FSM1AcceptingStates,
+  FSM1InitialState,
+  Seq(FSM1InitialState),
+  None
 )
 
 FSM1Instance = FSM1Instance.applyInput(anyToFSM1Input('a'))
 val FSM1StateAfterTransition1 = FSM1Instance.currentState
-FSM1Instance = FSM1Instance.applyInput(anyToFSM1Input('b'))    
+FSM1Instance = FSM1Instance.applyInput(anyToFSM1Input('a'))
 val FSM1StateAfterTransition2 = FSM1Instance.currentState
+val FSM1ComputedValueAfterTransition2 = FSM1Instance.computedValue
+FSM1Instance = FSM1Instance.applyInput(anyToFSM1Input('b'))    
+val FSM1StateAfterTransition3 = FSM1Instance.currentState
 println(FSM1StateAfterTransition1.identifier)
 println(FSM1StateAfterTransition2.identifier)
+println(FSM1ComputedValueAfterTransition2)
+println(FSM1StateAfterTransition3.identifier)
+
   
